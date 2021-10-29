c6e4eea800b03b624747601d2fee071d52f6fce0
Abdi-01 <abdialghi@gmail.com>
2021-10-29 13:26:25 +0700

    first commit

--- day04_loop.js
+++ day04_loop.js
@@ -9,11 +9,11 @@
  * } 
  */
 // dengan loop
-let count = 10;
-while (count > 0) {
-    console.log(count)
-    count--
-}
+// let count = 10;
+// while (count > 0) {
+//     console.log(count)
+//     count--
+// }
 
 // tanpa loop
 // console.log(10)
@@ -27,13 +27,96 @@ while (count > 0) {
 // console.log(2)
 // console.log(1)
 
-let number = 10;
-let res = "";
-while (number > 0) {
-    // task 1 : menambahkan ke variable res
-    res += `${number} `;
-    // task 2 : decrement number
-    number--
+// let number = 10;
+// let res = "";
+// while (number > 0) {
+//     // task 1 : menambahkan ke variable res
+//     res += `${number} `;
+//     // task 2 : decrement number
+//     number--
+// }
+
+// console.log(res)
+
+
+/**
+ * DO...WHILE LOOP
+ *
+ * do{
+ *      // program yang ingin dijalankan
+ * }while(condition)
+ *
+ * */
+
+// let value = 1;
+// do {
+//     console.log(value)
+//     value++
+// } while (value <= 10)
+
+
+/**
+ * FOR LOOP
+ * 
+ * for(statement_1;statement_2;statement3){
+ *      // Program yang ingin dijalankan
+ * 
+ * }
+ * 
+ * statement_ 1 : nilai awal yang digunakan untuk menjalankan looping
+ * statement_ 2 : condition yang dibuat untuk memeriksa apakah program dijalankan
+ * statement_ 3 : berisi proses increment atau decrement untuk memanipulasi nilai statement_1
+ *                sehingga nanti mempengaruhi condition pada statement_2
+ * 
+ * Keuntungannya : kita dapat membatasi jumlah loop sejak awal sesuai kebutuhan
+*/
+
+for (let val = 1; val <= 10; val++) {
+    if (val == 5) {
+        continue
+    } else {
+        console.log(val)
+    }
 }
 
-console.log(res)
+
+// for (let val = 1; val <= 30; val++) {
+//     if (val % 5 == 0 && val % 3 == 0) {
+//         console.log("BizzBuzz =>", val)
+//     } else if (val % 3 == 0) {
+//         console.log("Bizz =>", val)
+//     } else if (val % 5 == 0) {
+//         console.log("Buzz =>", val)
+//     }
+// }
+
+let n = 6;
+let str = "";
+let count = 0;
+for (let i = 0; i < n; i++) {
+    for (let i = 0; i < n; i++) {
+        count++
+        if(count<10){
+            str += `0${count} `
+        }else{
+            str += `${count} `
+        }
+    }
+    str += "\n"
+}
+
+/**
+ * loop 1
+ * str = str + "#\n"
+ * ==> str = "#\n"
+ * loop 2
+ * str = str + "#\n"
+ * ==> str = "#\n#\n"
+ * loop 3
+ * str = str + "#\n"
+ * ==> str = "#\n#\n#\n"
+ * */
+
+console.log(str)
+
+
---
+++ day05_array.js
@@ -0,0 +1,115 @@
+///////////////// ARRAY /////////////////////
+/**
+ * 1. Definis : Sebuah tipe data yang berisi sekumpulan data yang memiliki tipe data yang sama atau berbeda.
+ *              Dan setiap datanya itu memiliki index atau alamat.
+ *              Note : alamat index selalu dimulai dari 0 dan tipe datanya NUMBER INT.
+ * 2. Tujuan : untuk mempermudah pengelolaan data
+ * 3. Array bertipe Object
+ * 4. Array ditandai dengan adanya kurung siku [data]
+ * 5. Syntax :  keywordVariable namaVariable = [data_1, data2...]
+ */
+
+let buahApel = "Apel"; // ❌
+let buahJeruk = "Jeruk"; // ❌
+
+let buah = ["Apel", "Jeruk", "Buah Naga"]; // Cara 1️⃣
+console.table(buah);
+let stock = Array(15, 25, 10); // Cara 2️⃣
+console.log("Nama Stock =>", stock);
+let harga = []; // Cara 3️⃣
+harga[0] = 10000;
+harga[1] = 7500;
+harga[2] = 15000;
+harga[10] = 3500;
+console.log("Nama Harga =>", harga);
+
+// Cara memanggila data dari array : namaVariablenya[index_datanya]
+console.log(buah[0])
+console.log(buah[0] + " Stock = " + stock[0])
+// mengganti data array dengan re-assign.
+buah[0] = "Durian";
+console.log(`Buah ${buah[0]}, stocknya ada ${stock[0]} dan harganya Rp. ${harga[0].toLocaleString()}`)
+
+
+///////////////////////////////////// ARRAY FUNCTION ///////////////////////////////////////////
+
+// ⏺️ namaVariableArray.length : untuk mengetahui jumlah data pada array yang dimiliki suatu variable
+// output dari length adalah tipe data ⚠️⚠️⚠️NUMBER INT⚠️⚠️⚠️
+console.log("Banyak data array buah ==>", buah.length)
+console.log("Banyak data array stock ==>", stock.length)
+console.log("Banyak data array harga ==>", harga.length)
+console.log("ambil data terakhir =>", buah[buah.length - 1]); // buah[buah.length - 1] ===> buah[2] 
+console.log("ambil data terakhir =>", stock[stock.length - 1])
+
+// ⏺️ namaVariableArray.push(data) : digunakan utk menambah item baru diakhir array
+buah.push("Melon")
+stock.push(30)
+console.log(buah)
+console.log(stock)
+
+// ⏺️ namaVariableArray.pop() : menghapus item terakhir dari data array
+buah.pop();
+stock.pop();
+console.log(buah)
+console.log(stock)
+
+// ⏺️ namaVariableArray.unshift(data) : digunakan utk menambah item baru diawal array
+buah.unshift("Kurma");
+stock.unshift(50);
+console.log(buah)
+console.log(stock)
+
+// ⏺️ namaVariableArray.shift() : menghapus item pertama dari data array
+buah.shift()
+stock.shift()
+console.log(buah)
+console.log(stock)
+
+// ⏺️ namaVariableArray.splice(indexAwal, jumlahDataYangDihapus, dataBaru) : menghapus data array pada index tertentu
+// Fungsi 1️⃣ : menghapus data tertentu pada array
+let mobil = ["Toyota", "Daihatsu", "Nissan", "Lexus"];
+console.log("❌ Sebelum dihapus : ", mobil)
+mobil.splice(2, 1)
+console.log("✅ Sesudah dihapus : ", mobil)
+
+// Fungsi 2️⃣ : menyisipkan data baru
+console.log("❌ Sebelum disisipkan : ", mobil)
+mobil.splice(1, 0, "Mazda", "Ferrari")
+console.log("✅ Sesudah disisipkan : ", mobil)
+
+// Fungsi 3️⃣ : mengganti data
+console.log("❌ Sebelum diganti : ", mobil)
+mobil.splice(0, 1, "Tesla")
+console.log("✅ Sesudah diganti : ", mobil)
+
+// ⏺️ namaVariableArray.slice("start index", "end index")
+let motor = ["Yamaha", "Honda", "Kawasaki", "HasQ", "Suzuki"]
+console.log(motor.slice(1, 3))
+
+// ⏺️ namaVariableArray.includes("data") : memeriksa data ada pada array atau tidak
+console.log(motor.includes("Yamaha")); // true
+console.log(motor.includes("Suzuki")); // false
+
+// ⏺️ namaVariableArray.indexOf("data") : mencari alamat index dari suatu data didalam array
+console.log(motor.indexOf("HasQ")); // 3
+console.log(motor.indexOf("Suzuki")); // -1
+
+// ⏺️ namaVariableArray.reverse() : membalikkan urutan data array
+console.log("Before =>", motor)
+motor.reverse()
+console.log("After =>", motor)
+
+// ⏺️ namaVariableArray.join("separator") : menggabungkan data array menjadi sebuah data string
+let hari = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"]
+
+console.log(hari.join())
+console.log(hari.join(""))
+console.log(hari.join(" - "))
+
+// ⏺️ namaVariableArray.concat(dataArray,dataArray,...) : menggabungkan beberapa data array menjadi sebuah array baru
+// let kendaraan = mobil.concat(motor, ["Kereta", "Bus", "Pesawat"])
+// console.table(kendaraan)
+
+// Spread operator
+let kendaraan = [...mobil, ...motor, "Kereta", "Bus", "Pesawat"]
+console.table(kendaraan)
